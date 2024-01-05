3082d87d19107ebffbc61f6a12a9fe4acae3658f
Olta <oltaabytyqi@gmail.com>
2024-01-05 03:23:46 +0100

    Added styles

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Pokémons NFTs</title>
+</head>
+
+<div>
+</div>
+
+<div class="full-width-corner blue"></div>
+<div class="full-width-corner2 blue"></div>
+<img src="pictures/logo.png" alt="Logo" class="logo">
+
+<!-- <form action="login.php" method="post">
+            Username: <input type="text" name="un"> <br>
+            Password: <input type="password" name="pw"> <br>
+            <button type="submit">Login</button>
+        </form> -->
+
+        </body>
+</html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice 12.</title>
+</head>
+<form action="login.php" method="post">
+            Username: <input type="text" name="un"> <br>
+            Password: <input type="password" name="pw"> <br>
+            <button type="submit">Login</button>
+        </form>
+
+        </body>
+</html>

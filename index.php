1ef6307d383dc94ed705ce0137e573a2c48b5378
Olta <oltaabytyqi@gmail.com>
2024-01-05 17:14:26 +0100

    Added Pictures with Sats

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+$jsonString = file_get_contents('cards.json');
+$data = json_decode($jsonString, true);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +19,27 @@
 <div class="full-width-corner blue"></div>
 <div class="full-width-corner2 blue"></div>
 <img src="pictures/logo.png" alt="Logo" class="logo">
+
+<button class="button-style2">Login</button>
+<button class="button-style1">Register</button>
+
+
+<div class="cards-container">
+        <?php foreach($data as $card): ?>
+            <div class="card">
+                <img src="<?= htmlspecialchars($card['image']) ?>" alt="<?= htmlspecialchars($card['name']) ?>">
+                <div class="card-info">
+                    <h2 class="card-title"><?= htmlspecialchars($card['name']) ?></h2>
+                    <h3 class="card-type"><?= htmlspecialchars($card['type']) ?></h3>
+                    <h3 class="card-stats"><?= htmlspecialchars($card['hp']) ." ". htmlspecialchars($card['attack']) ." ". htmlspecialchars($card['defense'])?></h3>
+                    <h3> </h3>
+                    <h3 class="card-price"><?= htmlspecialchars($card['price']) ?></h3> 
+                    <!-- Display types, stats, etc. here -->
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
 
 <!-- <form action="login.php" method="post">
             Username: <input type="text" name="un"> <br>
